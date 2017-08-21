ef37580e4f07cc20b609d797b8309f321f17e779
fredGui <572752023@qq.com>
2017-08-21 22:21:12 +0800

    1

---
+++ modules/user/controller/ShowController.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fred
+ * Date: 2017/8/15
+ * Time: 22:00
+ */
+namespace app\modules\user\controller;
+use yii\base\Controller;
+
+class ShowController extends Controller{
+
+
+    public function actionIndex(){
+
+        echo 123;exit;
+    }
+}
