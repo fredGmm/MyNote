0bed0e9045be363d653f5ff591f00b0351029a57
guimingming <17771869383@163.com>
2018-04-11 17:26:17 +0800

    加上样式文件

--- Application/modules/ssnh/views/index/Index.php
+++ Application/modules/ssnh/views/index/Index.php
@@ -17,7 +17,7 @@
                     <meta name="apple-mobile-web-app-status-bar-style" content="black">
                     <meta name="apple-mobile-web-app-capable" content="yes">
                     <meta name="format-detection" content="telephone=no">
-                    <link rel="stylesheet" type="text/css" href="/src/css/ssnh/style.css"/>
+                    <link rel="stylesheet" type="text/css" href="/static/css/style.css"/>
                 </head>
                 <body class="childrenBody">
                 <!-- 上方内容 -->
